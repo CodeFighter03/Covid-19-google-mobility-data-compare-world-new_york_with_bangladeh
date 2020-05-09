344dc753946b811c3bff137270bdb4210b4769c5
Sazin Reshed Samin <53570377+CodeFighter03@users.noreply.github.com>
2020-05-09 10:29:44 +0600

    Add files via upload

---
+++ covid-2.py
@@ -0,0 +1,36 @@
+def cal(per):
+    per1=per/100
+    print(per1)
+
+
+
+print('\nBangladesh population density for 1% : ')
+population_density_per_km=1115.62
+cal(population_density_per_km)
+
+print('\nUnited State population density for 1% : ')
+population_density_per_km=94
+cal(population_density_per_km)
+
+print('\nEngland population density for 1% : ')
+population_density_per_km=259
+cal(population_density_per_km)
+
+print('\nItaly population density for 1% : ')
+population_density_per_km=533
+cal(population_density_per_km)
+
+print('\nFrance population density for 1% : ')
+population_density_per_km=117.37
+cal(population_density_per_km)
+
+print('\nDhaka population density for 1% : ')
+population_density_per_km=68561
+cal(population_density_per_km)
+
+print('\nNew York population density for 1% : ')
+population_density_per_km=27000
+cal(population_density_per_km)
+
+
+
